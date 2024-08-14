f10bfa1cf341efc3a6107695eaabc55f82d8a205
Trong Bui <145566680+TrongMinhBui@users.noreply.github.com>
2024-08-14 14:44:39 +0200

    First commit

---
+++ NetCourse/Assets/Scripts/NetworkManagerGui.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkManagerGui : MonoBehaviour
+{
+    [SerializeField] private NetworkManager _networkManager;
+    
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Host"))
+        {
+            _networkManager.StartHost();
+        }
+        
+        if (GUILayout.Button("Join"))
+        {
+            _networkManager.StartClient();
+        } 
+        
+        if (GUILayout.Button("Quit"))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ NetCourse/Assets/Scripts/input/InputReader.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "InputReader", menuName = "Game/Input Reader")]
+public class InputReader : ScriptableObject, GameInput.IGameplayActions
+{
+
+    GameInput gameInput;
+
+    public event UnityAction<Vector2> MoveEvent = delegate { };
+
+    public event UnityAction shootEvent = delegate { };
+
+    public event UnityAction<Vector2> mouseEvent = delegate { };
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent.Invoke(context.ReadValue<Vector2>());
+        throw new System.NotImplementedException();
+    }
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            shootEvent.Invoke();
+        }
+    }
+
+    public void OnMouse(InputAction.CallbackContext context)
+    {
+       mouseEvent.Invoke(context.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        if (gameInput == null)
+        {
+            gameInput = new GameInput();
+            gameInput.Gameplay.SetCallbacks(this);
+            gameInput.Gameplay.Enable();
+        }
+    }
+}
